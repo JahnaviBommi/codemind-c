8accdb214e74814c91c132d8919060deba37bba4
JahnaviBommi <98727361+JahnaviBommi@users.noreply.github.com>
2022-04-26 10:40:32 -0700

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,16 +5,11 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-         for(j=1;j<=n-2;j++)
-        {
-            printf("%d",j);
-        }
-        for(j=1;j<=n-3;j++)
+        for(j=1;j<=i;j++)
         {
             printf("%d",j);
         }
         printf("
 ");
-       
     }
 }
